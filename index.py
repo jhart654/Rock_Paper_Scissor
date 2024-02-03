73a8f8bb03566dc1ff5fe40308258412016fcd37
jhart654 <143129762+jhart654@users.noreply.github.com>
2024-02-03 15:01:37 -0500

    Add files via upload

--- index.py
+++ index.py
@@ -1,45 +1,50 @@
 import random
 
-Computer = random.randint(1, 3)
-Player = random.randint(1, 3)
+while True:
 
-if Computer == 1:
-    print('Computer: Rock')
-elif Computer == 2:
-    print('Computer: Paper')
-elif Computer == 3:
-    print('Computer: Scissor')
+    endGame = input('Would you like to play? (yes/no) ').lower()
 
-if Player == 1:
-    print('Player: Rock')
-elif Player == 2:
-    print('Player: Paper')
-elif Player == 3:
-    print('Player: Scissor')
+    if endGame == 'yes':
+        Player = input('Rock, Paper, Scissor? ').lower()
+        Computer = random.randint(1, 3)
 
-if Computer == 1 and Player == 1:
-    print('Tie')
-elif Computer == 2 and Player == 2:
-    print('Tie')
-elif Computer == 3 and Player == 3:
-    print('Tie')
-elif Computer == 1 and Player == 2:
-    print('Player wins')
-elif Computer == 1 and Player == 3:
-    print('Computer wins')
-elif Computer == 2 and Player == 1:
-    print('Computer wins')
-elif Computer == 2 and Player == 3:
-    print('Player wins')
-elif Computer == 3 and Player == 1:
-    print('Player wins')
-elif Computer == 3 and Player == 2:
-    print('Computer wins')
+        if Computer == 1:
+            print('Computer: Rock')
+        elif Computer == 2:
+            print('Computer: Paper')
+        elif Computer == 3:
+            print('Computer: Scissor')
 
+        if Player == 'rock':
+            print('Player: Rock')
+        elif Player == 'paper':
+            print('Player: Paper')
+        elif Player == 'scissor':
+            print('Player: Scissor')
 
+        if Computer == 1 and Player == 'rock':
+                print('Tie')
+        elif Computer == 2 and Player == 'paper':
+            print('Tie')
+        elif Computer == 3 and Player == 'scissor':
+            print('Tie')
+        elif Computer == 1 and Player == 'paper':
+            print('Player wins')
+        elif Computer == 1 and Player == 'scissor':
+            print('Computer wins')
+        elif Computer == 2 and Player == 'rock':
+            print('Computer wins')
+        elif Computer == 2 and Player == 'scissor':
+            print('Player wins')
+        elif Computer == 3 and Player == 'rock':
+            print('Player wins')
+        elif Computer == 3 and Player == 'paper':
+            print('Computer wins')
 
-
-
+    elif endGame == 'no':
+        print('Thank you for playing!!')
+        break
+        
 
 
 
